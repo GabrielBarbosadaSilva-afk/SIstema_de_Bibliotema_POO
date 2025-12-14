aea7ac4c8db8a0cad21f41a598011b6cd66497a8
Vitor <vvpt1@aluno.ifal.edu.br>
2025-12-14 15:50:33 -0300

    Criação da pasta menu

---
+++ SB_POO/menu.py
@@ -0,0 +1,6 @@
+from livro import Livro as Li
+from usuario import Usuario as Us
+from biblioteca import Biblioteca as Bib
+from emprestimos import Emprestimo as Emp
+
+#Aqui a gente tem que fazer uma imterface legível
